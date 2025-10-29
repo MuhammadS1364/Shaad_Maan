47f5d1a9d981fd5cef976940d5b9e14cd61d2fa5
MuhammadS1364 <shafeealam1364@outlook.com>
2025-10-29 12:55:05 +0530

    wings Model ready

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Wings_Model(models.Model):
+    wing_id = models.CharField(max_length=50, primary_key=True, unique=True)
+    wing_name = models.CharField(max_length=250, unique=True)
+    wing_chairPerson = models.CharField(max_length=250, unique=True)
+    wing_helper = models.CharField(max_length=205, unique=True)
